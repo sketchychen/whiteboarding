ad3ecc33ab021c5ea42d22be9a913a2fec3d465a
skylocke <mathematical.fiction@gmail.com>
2017-04-28 14:19:43 -0700

    Check Permutation .py and .js functions done

--- arraysAndStrings.js
+++ arraysAndStrings.js
@@ -1,5 +1,17 @@
+function assertEquals(testResult, expectedResult) {
+  if (testResult === expectedResult) {
+    return "Test passed.";
+  } else {
+    return "Test failed.";
+  }
+}
+
+// Is Unique:
+// Implement an algorithm to determine if a string has all unique characters.
+// What if you cannot use additional data structures?
 function isUnique(str) {
   var counts = {};
+
   str.split("").forEach(function(char) {
     if (counts.hasOwnProperty(char)) {
       counts[char]++;
@@ -15,11 +27,38 @@ function isUnique(str) {
   }
 
   return true;
-
 }
 
-var isUniqueTest1 = 'a;sdklfj';
-var isUniqueTest2 = 'asdf;lkja';
+console.log("isUniqueTest1: ", assertEquals(isUnique('a;sdklfj'), true));
+console.log("isUniqueTest2: ", assertEquals(isUnique('asdf;lkja'), false));
 
-console.log('isUniqueTest1: ', isUnique(isUniqueTest1));
-console.log('isUniqueTest2: ', isUnique(isUniqueTest2));
+// Check Permutation:
+// Given two strings, write a method to decide if one is a permutation of the other.
+function countChars(str) {
+  var counts = {};
+  str.split("").forEach(function(char) {
+    if (counts.hasOwnProperty(char)) {
+      counts[char]++;
+    } else {
+      counts[char] = 1;
+    }
+  });
+  return counts;
+}
+
+function checkPermutation(str1, str2) {
+  if (str1.length !== str2.length) { return false; }
+
+  var counts1 = countChars(str1);
+  var counts2 = countChars(str2);
+
+  for (var key in counts1) {
+    if (!counts2.hasOwnProperty(key)) { return false; }
+    else if (counts1[key] !== counts2[key]) { return false; }
+  }
+
+  return true;
+}
+
+console.log("checkPermutationTest1: ", assertEquals(checkPermutation('a;sdklfj', 'jkl;asdf'), true));
+console.log("checkPermutationTest2: ", assertEquals(checkPermutation('asdf;lkja', 'a;sdkfajd'), false));
--- arrays_and_strings.py
+++ arrays_and_strings.py
@@ -1,5 +1,14 @@
 from collections import defaultdict
 
+def assert_equals(test_result, expected_result):
+    if test_result == expected_result:
+        return "Test passed."
+    else:
+        return "Test failed."
+
+# Is Unique:
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you cannot use additional data structures?
 def is_unique(ss):
     counts = defaultdict(int)
     for char in list(ss):
@@ -10,8 +19,31 @@ def is_unique(ss):
             return False
     return True
 
-is_unique_test_1 = 'a;sdklfj'
-is_unique_test_2 = 'asdf;lkja'
+print('is_unique_test_1: ', assert_equals(is_unique('a;sdklfj'), True))
+print('is_unique_test_2: ', assert_equals(is_unique('asdf;lkja'), False))
 
-print('is_unique_test_1: ', is_unique(is_unique_test_1))
-print('is_unique_test_2: ', is_unique(is_unique_test_2))
+
+# Check Permutation:
+# Given two strings, write a method to decide if one is a permutation of the other.
+
+def check_counts(ss):
+    counts = defaultdict(int)
+    for char in list(ss):
+        counts[char] += 1
+    return counts
+
+def check_permutation(ss1, ss2):
+    if len(ss1) != len(ss2):
+        return False
+
+    counts1 = check_counts(ss1)
+    counts2 = check_counts(ss2)
+
+    for key, value in counts1.items():
+        if key not in counts2:
+            return False
+
+    return True
+
+print("check_permutation_test1: ", assert_equals(check_permutation('a;sdklfj', 'jkl;asdf'), True));
+print("check_permutation_test2: ", assert_equals(check_permutation('asdf;lkja', 'a;sdkfajd'), False));
